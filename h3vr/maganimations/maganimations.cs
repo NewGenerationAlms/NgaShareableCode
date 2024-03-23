629617e70c3d9ef408f034386bf7688f948d9fc2
NG Alms <120841390+NewGenerationAlms@users.noreply.github.com>
2024-03-22 20:47:46 -0700

    Create maganimations.cs

---
+++ h3vr/maganimations/maganimations.cs
@@ -0,0 +1,156 @@
+using BepInEx;
+using BepInEx.Logging;
+using FistVR;
+using UnityEngine;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace NGA
+{
+    //[BepInAutoPlugin]
+    [BepInPlugin("NGA.MagAnimations", "MagAnimations", "0.1.0")]
+    [BepInDependency("nrgill28.Sodalite", "1.4.1")]
+    [BepInProcess("h3vr.exe")]
+    public partial class MagAnimations : BaseUnityPlugin
+    {
+        /* == Quick Start == 
+         * Your plugin class is a Unity MonoBehaviour that gets added to a global game object when the game starts.
+         * You should use Awake to initialize yourself, read configs, register stuff, etc.
+         * If you need to use Update or other Unity event methods those will work too.
+         *
+         * Some references on how to do various things:
+         * Adding config settings to your plugin: https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/4_configuration.html
+         * Hooking / Patching game methods: https://harmony.pardeike.net/articles/patching.html
+         * Also check out the Unity documentation: https://docs.unity3d.com/560/Documentation/ScriptReference/index.html
+         * And the C# documentation: https://learn.microsoft.com/en-us/dotnet/csharp/
+         */
+
+        // Player-Configurable Vars.
+        private static ConfigEntry<bool> config_enable;
+        private static ConfigEntry<float> config_mag_lock_dist;
+        //private static ConfigEntry<float> config_mag_lock_hozdist;
+        private static ConfigEntry<float> config_mag_auto_load_above;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+
+            Harmony harmony = new Harmony("NGA.MagAnimations");
+            Logger.LogMessage("New harmony");
+            SetUpConfigFields();
+            Logger.LogMessage("Setted the fields");
+            harmony.PatchAll();
+            Logger.LogMessage($"Hello, world! Sent from NGA.MagAnimations 0.0.1");
+        }
+
+        // Assigns player-set variables.
+        private void SetUpConfigFields()
+        {
+            config_enable = Config.Bind("Magazine",
+                                         "ON/OFF",
+                                         true,
+                                         "Enable or disable feature.");
+            config_mag_lock_dist = Config.Bind("Magazine",
+                                         "Vertical Distance to start mag insert animation",
+                                         0.15f,
+                                         "How far before mag starts insert animation in vertical dir");
+            // config_mag_lock_hozdist = Config.Bind("Magazine",
+            //                              "Horizontal Distance to start mag insert animation",
+            //                              0.05f,
+            //                              "How far before mag starts insert animation on horizontal dir");
+            config_mag_auto_load_above = Config.Bind("Magazine",
+                                         "Auto Load distance above entered",
+                                         0.02f,
+                                         "How far above mag entry point is needed to push to auto load.");
+        }
+
+        
+        [HarmonyPatch(typeof(FVRFireArmMagazine))]
+		[HarmonyPatch("FVRFixedUpdate")]
+		class FVRFireArmMagazineFVRFixedUpdate
+		{
+            
+            static void Postfix(FVRFireArmMagazine __instance) 
+            {
+                if (!config_enable.Value) {
+                    return;
+                }
+                if (__instance.IsHeld && __instance.m_hand.OtherHand.CurrentInteractable != null 
+                    && __instance.m_hand.OtherHand.CurrentInteractable is FVRFireArm)
+                {
+                    FVRFireArm fvrfireArm = __instance.m_hand.OtherHand.CurrentInteractable as FVRFireArm;
+                    Transform hole = fvrfireArm.GetMagMountPos(__instance.IsBeltBox);
+                    bool isMagPalming = __instance.m_magChild != null || __instance.m_magParent != null;
+                    if (fvrfireArm.Magazine == null  && fvrfireArm.MagazineType == __instance.MagazineType 
+                        && fvrfireArm.GetMagMountPos(__instance.IsBeltBox) != null
+                        && !isMagPalming)
+                    {
+                        // Force easy load.
+                        __instance.SetAllCollidersToLayer(false, "NoCol");
+						__instance.IsNonPhysForLoad = true;
+
+                        // Do calcs.
+                        Vector3 localUp = hole.up;
+                        if (__instance.gameObject.name == "P90_Mag(Clone)"
+                            || __instance.gameObject.name == "LaserPistol_Magazine(Clone)") {
+                            return;
+                        }
+                        Vector3 toHole = __instance.RoundEjectionPos.position - hole.position;
+                        float horizontalDist = Vector3.ProjectOnPlane(toHole, localUp).magnitude;
+                        float upDist = DistanceAlongUpAxis(__instance.transform, hole);
+                        float dist = Vector3.Distance(__instance.RoundEjectionPos.position, hole.position);
+                        if (dist <= config_mag_lock_dist.Value 
+                            ) //&& horizontalDist <= config_mag_lock_hozdist.Value && upDist >= 0
+                        {
+                            // Calculate the aligned position below the hole
+                            Vector3 alignedPosition = hole.position - localUp * upDist;
+                            __instance.Viz.position = alignedPosition;
+                            __instance.Viz.rotation = hole.rotation;
+
+                            if (upDist < -config_mag_auto_load_above.Value) {
+                                // Actual load.
+                                __instance.Load(fvrfireArm);
+                            }
+                        }
+                        else
+                        {
+                            // undo when not close
+                            __instance.Viz.position = __instance.transform.position;
+                            __instance.Viz.rotation = __instance.transform.rotation;
+                            // Force easy load.
+                            __instance.SetAllCollidersToLayer(false, "Default");
+						    __instance.IsNonPhysForLoad = false;
+                        }
+                    } 
+                }
+                if (__instance.FireArm) {
+                    FVRFireArm fvrfireArm = __instance.FireArm;
+                    Transform hole = fvrfireArm.GetMagMountPos(__instance.IsBeltBox);
+                    __instance.Viz.position = hole.position;
+                    __instance.Viz.rotation = hole.rotation;
+                    __instance.transform.position = hole.position;
+                    __instance.transform.rotation = hole.rotation;
+                }
+            }
+            static private float DistanceAlongUpAxis(Transform mag, Transform hole)
+            {
+                // Calculate the vector from the hole to the mag
+                Vector3 toMag = hole.position - mag.position;
+
+                // Get the hole's local up direction
+                Vector3 holeUp = hole.up;
+
+                // Project the vector onto the hole's up direction
+                float distanceAlongUp = Vector3.Dot(toMag, holeUp);
+
+                return distanceAlongUp;
+            }
+        }
+
+        // The line below allows access to your plugin's logger from anywhere in your code, including outside of this file.
+        // Use it with 'YourPlugin.Logger.LogInfo(message)' (or any of the other Log* methods)
+
+        internal new static ManualLogSource Logger { get; private set; }
+
+    }
+}
